5d3c01aa769ce7025ed671bf20711e044a2a42aa
miraclehimself <aderiyebiola@gmail.com>
2024-05-03 23:03:38 +0100

    Scheduler bug fixed

--- users/management/commands/subscription.py
+++ users/management/commands/subscription.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from users.models import User
 from django.utils.dateparse import parse_datetime
+from datetime import datetime, timedelta
 
 class Command(BaseCommand):
     help = 'Updates Subscription Status'
@@ -9,12 +10,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Get all users with a subscription
         users_with_subscription = User.objects.filter(expired=False)
+        # print(users_with_subscription)
         for user in users_with_subscription:
-            if(timezone.now() > user.subscription_due_date):
-                user.subscription_status = 'EXPIRED'
-                user.expired = True
-                user.save()
-            
+            if user.subscription_due_date != None:
+                if timezone.now() > user.subscription_due_date :  
+                    user.subscription_status = 'EXPIRED'
+                    user.expired = True
+                    user.save()
+      
             self.stdout.write(self.style.SUCCESS('Subscription status updated successfully'))
                 
         
