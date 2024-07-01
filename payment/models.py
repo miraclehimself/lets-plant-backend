e3afa37df4c1e82757dff13e2d4f939f6c2acbf6
miraclehimself <aderiyebiola@gmail.com>
2024-07-01 17:09:59 +0100

    Adjustment made to Gocardless

--- payment/models.py
+++ payment/models.py
@@ -9,3 +9,22 @@ class Payment(models.Model):
     successful = models.BooleanField(null=True, default=False)
     meta_data = models.JSONField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gocardless_customer_id = models.CharField(max_length=255, blank=True, null=True)
+    mandate_id = models.CharField(max_length=255, blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+class Subscription(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    subscription_type = models.CharField(max_length=100)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.subscription_type} for {self.customer.user.username}"
--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .models import Payment
+from .models import Payment, Customer
 import requests
 from django.conf import settings
 from django.http import JsonResponse
@@ -24,6 +24,19 @@ import gocardless_pro
 def makePayment(request):
     user = request.user
     client = gocardless_pro.Client(access_token=settings.GC_TOKEN, environment='sandbox')
+    customer, created = Customer.objects.get_or_create(user=user)
+    if not customer.gocardless_customer_id:
+            # Create a GoCardless customer if not exists
+            gc_customer = client.customers.create(params={
+                'given_name': user.name,
+                'family_name': user.name,
+                'email': user.email,
+                'metadata': {
+                    'user_id': str(user.id)
+                }
+            })
+            customer.gocardless_customer_id = gc_customer.id
+            customer.save()
     try:
         billing_request = client.billing_requests.create(params={
             "payment_request": {
@@ -32,6 +45,9 @@ def makePayment(request):
                 "description": "LetPlant",
                 "app_fee": "500",
             },  
+            "mandate_request": {
+            "scheme": "bacs"
+            }
         })
        
         billing_request_data = {
@@ -44,15 +60,19 @@ def makePayment(request):
                 "description": billing_request.payment_request.description,
                 # Include other relevant attributes of PaymentRequest
             },
+            
             # Add other relevant attributes
         }
+        # return JsonResponse(billing_request_data)
+
         try:
             client = gocardless_pro.Client(access_token=settings.GC_TOKEN, environment='sandbox')
             billingflow = client.billing_request_flows.create(params={
             "redirect_uri": "https://letsplant.com/",
             "exit_uri": "https://letsplant.com/",
             "links": {
-                "billing_request": billing_request.id
+                "billing_request": billing_request.id,
+                "customer": customer.gocardless_customer_id
             }
             })
             billing_details = {
